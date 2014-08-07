16a6c4d24ef9c8dd1eb90816f2e8c50710d2df8a
Adam Magaluk <AdamMagaluK@gmail.com>
2014-08-07 16:00:31 -0400

    Cleaned up scout

--- devices/index.js
+++ devices/index.js
@@ -7,25 +7,12 @@ var Pins = {
   MotionSensor: 'P8_19'
 };
 
-
 var BeagleBone = module.exports = function() {
   Scout.call(this);
 };
 util.inherits(BeagleBone, Scout);
 
 BeagleBone.prototype.init = function(cb) {
-  this.create('motion-sensor', MotionSensor, Pins.MotionSensor);
+  this.discover(MotionSensor, Pins.MotionSensor);
   cb();
-};
-
-BeagleBone.prototype.create = function(name, Type) {
-  var self = this;
-  var query = this.server.where({ type: name });
-  this.server.find(query, function(err, results){
-    if (results.length > 0) {
-      self.provision(results[0], Type);
-    } else {
-      self.discover(Type);
-    }
-  };
 };
